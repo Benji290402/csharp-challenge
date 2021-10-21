8e37dd23bb281509b1fb310ff0db766462507faf
BVIAL--SIMON <bvial--simon@noreply.git.ytrack.learn.ynov.com>
2021-10-21 13:58:52 +0000

    Add 'SetPiValue.cs'

---
+++ SetPiValue.cs
@@ -0,0 +1,13 @@
+using System;
+namespace CSharpDiscovery.Quest01
+{
+    public class SetPiValue_Exercice
+    {
+        public static float SetPiValue(float pi)
+        {
+            double a = 3.14159;
+            float b = Convert.ToSingle(a);
+            return (b);
+        }
+    }
+}
